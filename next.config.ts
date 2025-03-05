3f3c7ccb71e73bcb8c22fdd73f9e1b768b256de3
MutheuMC <cheldeanmutheu1@gmail.com>
2025-03-06 02:43:28 +0300

    added filtering

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint checks during builds
+  },
 };
 
 export default nextConfig;
